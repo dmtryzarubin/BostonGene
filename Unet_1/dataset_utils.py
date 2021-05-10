306f2bf032bc085d642fe5725ec5e37129841537
dmtryzarubin <dmtryzarubin@mail.ru>
2021-05-10 11:49:59 +0300

    first commit

---
+++ Unet_1/dataset_utils.py
@@ -0,0 +1,143 @@
+import os
+import shutil
+
+import numpy as np
+import torch
+import torch.utils.data
+from PIL import Image
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from data_utils import generate_random_data, masks_to_colorimg
+
+
+
+def create_dataset(height, width, n_train, n_val, n_test, root):
+    """
+    Creates a two-folder dataset with images and masks
+    :params height, width: image height and width
+    :param n_imgs: number of Images in the dataset
+    :param ROOT: Root folder
+    """
+    folder = 'Segmentation_Data'
+    path = os.path.join(root, folder)
+    sets = ['Train', 'Val', 'Test']
+    subfolders = ['Images', 'Masks']
+    n = [n_train, n_val, n_test] 
+
+    def generate_n_save(path, n_imgs):
+        """
+        Generating n images (n_imgs) and parsing them into folders.
+        """
+        for n in tqdm(range(1, n_imgs + 1)):
+            img, masks = generate_random_data(height, width, 1)
+
+            
+            # Convert Image to RGB
+            img_rgb = [x.astype(np.uint8) for x in img][0]
+            # Each objects has its intensity. Ex: (index = 1 -> intensity = 1, etc)
+            # Get single channel
+            mask = np.max(masks[0], axis=0) 
+
+            im = Image.fromarray(img_rgb)
+            im.save(os.path.join(path, 'Images') + '\\img_' + str(n) + '.png')
+            ms = Image.fromarray(mask).convert("P")
+            ms.save(os.path.join(path, 'Masks') + '\\mask_' + str(n) + '.png')
+    
+    # Checking if dir already exists
+    if os.path.exists(path):
+            print(f'Directory |{path}|\nAlready exists! It will be removed')
+            shutil.rmtree(path, ignore_errors=True)
+
+    # Creating two directories: for masks and images.
+    
+    try:
+        os.makedirs(path)
+        for idx, s in enumerate(sets):
+            subset_path = os.path.join(path, s)
+            os.makedirs(subset_path)
+            for subfolder in subfolders:
+                subfolder_path = os.path.join(subset_path, subfolder)
+                os.makedirs(subfolder_path)
+            generate_n_save(subset_path, n[idx])
+            
+    except OSError:
+        print (f'Creation of the directory |{path}| failed')
+
+
+
+
+def plot_img_n_masks(root, num_imgs):
+    img_path = os.path.join(root,'Images')
+    masks_path = os.path.join(root,'Masks')
+    idxs = np.random.randint(low=1, high=len(os.listdir(img_path)), size=num_imgs)
+    fig, axs = plt.subplots(nrows=num_imgs, ncols=num_imgs, figsize=(7, 7), sharex='all', sharey='all')
+
+    for i in range(0, num_imgs):
+        img = Image.open(os.path.join(img_path,f'img_{idxs[i]}.png'))
+        mask = Image.open(os.path.join(masks_path,f'mask_{idxs[i]}.png'))
+        
+        img = np.array(img)
+        mask = np.array(mask)
+
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]
+        masks = mask == obj_ids[:, None, None] 
+        masks = masks.astype('float32')
+
+        axs[i, 0].imshow(img)
+        axs[i, 1].imshow(masks_to_colorimg(masks))
+    plt.suptitle('Images and Masks', fontsize="x-large")
+    plt.tight_layout()
+    plt.show()    
+
+
+
+
+class Segmentation_Dataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "Images"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "Masks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "Images", self.imgs[idx])
+        mask_path = os.path.join(self.root, "Masks", self.masks[idx])
+        
+        img = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path), dtype='float32')
+
+        reference = np.array([ 10., 20., 30., 40., 50., 60.])
+
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+        masks = masks.astype('float32')
+
+        if len(obj_ids) != len(reference):
+            missing_ch = list(set(reference) - set(obj_ids))
+            for i in sorted(missing_ch):
+                masks = np.insert(
+                     masks, 
+                     int(i // 10) - 1, 
+                     np.zeros((mask.shape[0], mask.shape[1]), dtype='float32'),
+                     axis=0)
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+            
+
+        return img, masks
+
+    def __len__(self):
+        return len(self.imgs)
---
+++ Unet_1/unet.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+
+def double_conv(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    """
+    Double-Convolution layer with RELU activation function
+    param: in_channels : number of input channels
+    param: out_channels : number of output channels
+    """
+    conv = nn.Sequential(
+        nn.Conv2d(
+            in_channels, 
+            out_channels, 
+            kernel_size=kernel_size, 
+            stride=stride, 
+            padding=padding,
+            bias=False
+            ),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(
+            out_channels, 
+            out_channels, 
+            kernel_size=kernel_size, 
+            stride=stride, 
+            padding=padding,
+            bias=False
+            ),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+    return conv
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        
+        self.max_pool_2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Left side of Unet
+        self.down_conv_1 = double_conv(in_channels=3, out_channels=16)#
+
+        self.down_conv_2 = double_conv(in_channels=16, out_channels=32)#
+        
+        self.down_conv_3 = double_conv(in_channels=32, out_channels=64)#
+        
+        self.down_conv_4 = double_conv(in_channels=64, out_channels=96)#
+        
+        self.down_conv_5 = double_conv(in_channels=96, out_channels=128)
+        
+        # Right side of Unet
+        self.up_trans_1 = nn.ConvTranspose2d(
+            in_channels=128,
+            out_channels=96,
+            kernel_size=2,
+            stride=2)
+        self.up_conv_1 = double_conv(96*2, 96)
+
+        self.up_trans_2 = nn.ConvTranspose2d(
+            in_channels=96,
+            out_channels=64,
+            kernel_size=2,
+            stride=2)
+        self.up_conv_2 = double_conv(128, 64)
+
+        self.up_trans_3 = nn.ConvTranspose2d(
+            in_channels=64,
+            out_channels=32,
+            kernel_size=2,
+            stride=2)
+        self.up_conv_3 = double_conv(64, 32)
+
+        self.up_trans_4 = nn.ConvTranspose2d(
+            in_channels=32,
+            out_channels=16,
+            kernel_size=2,
+            stride=2)
+        self.up_conv_4 = double_conv(32, 16)
+
+        self.out = nn.Conv2d(
+            in_channels=16,
+            out_channels=6,
+            kernel_size=1)
+    
+    def forward(self, image):
+        """
+        Implements forward pass
+        """
+        # Encoder
+
+        # Input image [3, 128, 128]
+        l_1 = self.down_conv_1(image)
+        # Out shape: [16, 128, 128]
+        l_2 = self.max_pool_2(l_1)
+        # [16, 64, 64]
+        l_3 = self.down_conv_2(l_2)
+        # [32, 64, 64]
+        l_4 = self.max_pool_2(l_3)
+        # [32, 32, 32]
+        l_5 = self.down_conv_3(l_4) 
+        # [64, 32, 32]
+        l_6 = self.max_pool_2(l_5)
+        # [64, 16, 16]
+        l_7 = self.down_conv_4(l_6) 
+        # [96, 16, 16]
+        l_8 = self.max_pool_2(l_7)
+        # [96, 8, 8]
+        l_9 = self.down_conv_5(l_8)
+        # [128, 8, 8]
+        
+        # Decoder
+        l_10 = self.up_trans_1(l_9)
+        # [96, 16, 16]
+        l_11 = self.up_conv_1(torch.cat([l_10, l_7], axis=1))
+        # [96, 16, 16]
+        l_12 = self.up_trans_2(l_11)
+        # [64, 32, 32]
+        l_13 = self.up_conv_2(torch.cat([l_12, l_5], axis=1))
+        # [64, 32, 32]
+        l_14 = self.up_trans_3(l_13)
+        # [64, 32, 32]
+        l_15 = self.up_conv_3(torch.cat([l_14, l_3], axis=1))
+        # [32, 64, 64]
+        l_16 = self.up_trans_4(l_15)
+        # [16, 128, 128]
+        l_17 = self.up_conv_4(torch.cat([l_16, l_1], axis=1))
+        # [16, 128, 128]
+        out = self.out(l_17)
+        # [6, 128, 128]
+        return out
+
+
+if __name__ == '__main__':
+    image = torch.rand((16, 3, 160, 160))
+    model = UNet()
+    print(model(image))
